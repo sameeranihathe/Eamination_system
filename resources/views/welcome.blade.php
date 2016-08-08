fa580f7b3fd39e29f158cba191cfa11ad6836b20
DanthakaSri <pasinduworks92@gmail.com>
2016-08-08 21:54:00 +0530

    home page changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,23 +1,98 @@
 <html>
 	<head>
-		<title>Laravel</title>
-		<script src="{{ asset('/js/search.js') }}"></script>
+		<title>Index_page</title>
+		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+		
+		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+		
+		<style>
+		  .buttonContainer{
+
+		  }
+		  .p{
+		  	font-size:2em;
+		  }
+
+			
+		</style>
 	</head>
 	<body>
-		<div class="container">
-			<div class="content">
-				<div class="form-group">
-					<label for="name" class="col-sm-2" control-label">Label:</label>
-					<div class="col-sm-10">
-						<input type="text" class="form-control" name="" id="search-input" onkeydown="down()" onkeyup="up()" placeholder="Input...">
-					</div>
-				</div>
+	<!--............navigation-bar.................................-->
+        <nav class="navbar navbar-inverse">
+        	<div class="container-fluid">
+        	<div class="navbar-header">
+        		<a class="navbar-brand" href="#">Home</a>
+        	</div>
+        	<ul class="nav navbar-nav">
+        		<li class="active"><a href="#">Home</a></li>
+        		<li><a href="#">Student</a></li>
+        		<li><a href="#">Lecturer</a></li>
+        	</ul>
+        	<ul class="nav navbar-nav navbar-right">
+        		<li><a href="#"><span class="glyphicon glyphicon-user"></span>Admin</a></li>
+        	</ul>
+        	</div>
+        </nav>
 
-				<div class="alert alert-info" id="search-result">
-					<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-					<strong>Title!</strong> Alert body ...
-				</div>
-			</div>
-		</div>
+    <!--............................image slider.....................-->
+		 <div class="slider">
+		 	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+ 
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="http://placehold.it/1200x315" alt="...">
+      <div class="carousel-caption">
+      	<h3>Caption Text</h3>
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://placehold.it/1200x315" alt="...">
+      <div class="carousel-caption">
+      	<h3>Caption Text</h3>
+      </div>
+    </div>
+    <div class="item">
+      <img src="http://placehold.it/1200x315" alt="...">
+      <div class="carousel-caption">
+      	<h3>Caption Text</h3>
+      </div>
+    </div>
+  </div>
+ 
+  <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left"></span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right"></span>
+        </a>
+        </div> <!-- Carousel -->
+        </div>
+</div>		 		
+		
+
+		 <div class="paragraphContainer">
+		 	<p>Now you can check your examination results as soon as they are released.Click below to check your results.</p>
+		 </div>
+
+		 <div class="buttonContainer">
+		 	<button type="button" class="btn btn-primary btn-lg">Check Results</button>
+		 </div>
+
+
+
+		
 	</body>
 </html>
